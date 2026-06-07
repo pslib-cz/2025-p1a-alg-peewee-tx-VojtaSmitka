1cee12e1e4c90d955ffd43f337ef075b0388fa53
vojtasmitka <vojtech.smitka.025@pslib.cz>
2026-06-07 20:58:58 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,49 @@
-// Empty MakeCode project
+radio.setGroup(25)
+let gas: number = 0
+const opakovani: number = 5
+let hodnota: number
+
+let gearIndex: number = 1
+const gear: number[] = [-30, 40]
+
+basic.showString(`${gearIndex}`)
+
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        gas = gear[gearIndex]
+    }
+    else if (input.buttonIsPressed(Button.B)) {
+        if (gearIndex < gear.length - 1) {
+            gearIndex = gearIndex + 1
+            basic.showString(`${gearIndex}`)
+            basic.pause(200)
+        } else {
+            gearIndex = 0
+
+            basic.showString(`${gearIndex}`)
+            basic.pause(200)
+        }
+    }
+    else {
+        gas = 0
+    }
+
+    hodnota = 0
+    for (let i = 0; i < opakovani; i++) {
+        hodnota += input.rotation(Rotation.Roll)
+        basic.pause(10)
+    }
+
+    hodnota = hodnota / opakovani
+
+    if (hodnota > 100) {
+        hodnota = 100
+    }
+    else if (hodnota < -100) {
+        hodnota = -100
+    }
+
+    radio.sendValue("forward", gas)
+    radio.sendValue("steer", hodnota)
+    basic.pause(20)
+})
